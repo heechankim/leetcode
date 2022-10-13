a708730943baed7475f5d70efdcc21678044e0fe
Heechan Kim <heechan.dev@gmail.com>
2022-10-13 19:46:30 +0900

    Time: 159 ms (13.26%), Space: 13.8 MB (96.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         s = str(x)
-        print(s, len(s))
+        backwardIndex = len(s) - 1
         for i in range(len(s)):
             
-            if (i == (len(s) - 1) - i ):
+            if (i == backwardIndex - i ):
                 break
             
-            if (s[i] != s[(len(s) - 1) - i]):
+            if (s[i] != s[backwardIndex - i]):
                 return False
         
         return True
