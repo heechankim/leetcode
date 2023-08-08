17c7cbd2924e09316ae091cb9918580931421974
Heechan Kim <heechan.dev@gmail.com>
2023-08-08 19:31:35 +0900

    Time: 2 ms (55.85%), Space: 2.4 MB (40.70%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.go
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -1,35 +1,12 @@
-func stringMerge(s1, s2 string) string {
-
-  var res string
-
-  for i := 0; i < len(s1); i++ {
-
-    res += string(s1[i]) + string(s2[i])
-
-  }
-
-  return res
-
-}
-
 func mergeAlternately(word1 string, word2 string) string {
-
-  switch {
-
-    case len(word1) == len(word2):
-
-      return stringMerge(word1, word2)
-
-    case len(word1) > len(word2):
-
-      return stringMerge(word1[:len(word2)], word2) + word1[len(word2):]
-
-    case len(word1) < len(word2):
-
-      return stringMerge(word1, word2[:len(word1)]) + word2[len(word1):]
-
+  
+  if len(word1) == 0 {
+    return word2
   }
-
-  return ""
-
+  
+  if len(word2) == 0 {
+    return word1
+  }
+  
+  return word1[:1] + word2[:1] + mergeAlternately(word1[1:], word2[1:])
 }
